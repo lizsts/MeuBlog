ac251f60d7940353f40c986d2be7597af08ba9bc
Liz St <lizstspsi@outlook.com>
2021-07-12 19:54:41 -0300

    ajuste de segurança

--- src/main/java/com/meublog/security/BasicSecurityConfig.java
+++ src/main/java/com/meublog/security/BasicSecurityConfig.java
@@ -30,7 +30,7 @@ public class BasicSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests()
-		.antMatchers("/usuario/logar").permitAll()
+		.antMatchers("/usuario/entrar").permitAll()
 		.antMatchers("/usuario/cadastrar").permitAll()
 		.anyRequest().authenticated()
 		.and().httpBasic()
